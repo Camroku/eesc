49c93176b841b27283839b88d6af6342ff16195b
Çınar Yılmaz <79412062+Comrade-Otaku@users.noreply.github.com>
2021-04-15 18:39:49 +0300

    Multilang support

--- eesc.py
+++ eesc.py
@@ -1,6 +1,32 @@
 # Easter Egg Supported Calculator
 processes = ["+", "-", "/", "*", "%"]
 easters = {}
+lang = "en"
+langs = ["tr", "en"]
+translation = {
+"en":{
+    "n_val_proc": "The defined process is not a valid process.",
+    "n_val_fnum": "The defined first number is not a valid number.",
+    "n_val_snum": "The defined second number is not a valid number.",
+    "i_fnum": "First Number: ",
+    "i_proc": "Process: ",
+    "i_snum": "Second Number: ",
+    "i_n_val_num": "The inputted value is not an integer.",
+    "i_n_val_proc": "The inputted value is not an process. The calculator supports only +, -, /, * and %.",
+    "out": "Output: "
+},
+"tr":{
+    "n_val_proc": "Tanımlanan işlem geçerli bir işlem değil.",
+    "n_val_fnum": "Tanımlanan ilk sayı geçerli bir sayı değil.",
+    "n_val_snum": "Tanımlanan ikinci sayı geçerli bir sayı değil.",
+    "i_fnum": "İlk Sayı: ",
+    "i_proc": "İşlem: ",
+    "i_snum": "İkinci Sayı: ",
+    "i_n_val_num": "Girilen değer bir sayı değil.",
+    "i_n_val_proc": "Girilen değer bir işlem değil. Hesap makinesi sadece +, -, /, * ve % \işlemlerini destekler.",
+    "out": "Çıktı: "
+}
+}
 def isInt(s):
     try: 
         int(s)
@@ -11,39 +37,45 @@ class notNumberException(Exception):
     pass
 class notProcessException(Exception):
     pass
+def setLang(lng:str):
+    global lang, langs
+    if lang in langs:
+        lang = lng
 def addEasterEgg(fnum:str, proc:str, snum:str, out:str):
+    global lang, translation
     if not proc in processes:
-        raise notProcessException(f"The defined process \"{proc}\" is not a valid process.")
+        raise notProcessException(translation[lang]["n_val_proc"])
     if not isInt(fnum):
-        raise notNumberException(f"The defined first number \"{fnum}\" is not a valid number.")
+        raise notNumberException(translation[lang]["n_val_fnum"])
     if not isInt(snum):
-        raise notNumberException(f"The defined second number \"{snum}\" is not a valid number.")
+        raise notNumberException(translation[lang]["n_val_snum"])
     easters.update({fnum + ":" + proc + ":" + snum:out})
 
 def runCalc():
+    global lang, translation
     run = True
-    a = input("First Number: ")
+    a = input(translation[lang]["i_fnum"])
     if not isInt(a):
-        print(f"The inputted value \"{a}\" is not an integer.")
+        print(translation[lang]["i_n_val_num"])
         exit()
-    b = input("Process: ")
+    b = input(translation[lang]["i_proc"])
     if not b in processes:
-        print(f"The inputted value \"{b}\" is not an process. It supports only +, -, /, * and %.")
+        print(translation[lang]["i_n_val_proc"])
         exit()
-    c = input("Second number: ")
+    c = input(translation[lang]["i_snum"])
     if not isInt(c):
-        print(f"The inputted value \"{c}\" is not an integer.")
+        print(translation[lang]["i_n_val_num"])
         exit()
     if a + ":" + b + ":" + c in easters.keys():
-        print("Output: " + easters[a + ":" + b + ":" + c])
+        print(translation[lang]["out"] + easters[a + ":" + b + ":" + c])
     else:
         if(b == "+"):
-            print("Output: " + str(int(a) + int(c)))
+            print(translation[lang]["out"] + str(int(a) + int(c)))
         if(b == "-"):
-            print("Output: " + str(int(a) - int(c)))
+            print(translation[lang]["out"] + str(int(a) - int(c)))
         if(b == "/"):
-            print("Output: " + str(int(a) / int(c)))
+            print(translation[lang]["out"] + str(int(a) / int(c)))
         if(b == "*"):
-            print("Output: " + str(int(a) * int(c)))
+            print(translation[lang]["out"] + str(int(a) * int(c)))
         if(b == "%"):
-            print("Output: " + str(int(a) % int(c)))
+            print(translation[lang]["out"] + str(int(a) % int(c)))
